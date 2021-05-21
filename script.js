f8f52b37087d33a8d844f8cd60a2ff9e6df02c32
everly <everlyprecia@gmail.com>
2021-05-21 19:36:40 +0530

    Add Javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      range = end - start,
+      inc = end > start ? 1 : -1,
+      count = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       start += inc;
+       obj.textContent = start ;
+       if (start == end) {
+        clearInterval(timer);
+       }
+      }, count);
+    }
+
+    
+   window.addEventListener('scroll',()=>{
+       let cont=document.querySelector('.f-h');
+       let anime=document.querySelectorAll('.reveal');
+       let size=window.innerHeight;
+       for(var i=0; i<anime.length;i++){
+           var top=anime[i].getBoundingClientRect().top;
+           if(top<size){
+               anime[i].classList.add('active');
+           }
+           else{
+               anime[i].classList.remove('active');
+           }
+       }
+       let contp=cont.getBoundingClientRect().top;
+       let screenp=window.innerHeight;
+       if (contp < screenp){
+        counter("count1", 2470, 2570, 1000);
+        counter("count2", 77, 177, 1000);
+        counter("count3", 1400, 1500, 1000);
+       }
+       else{
+           cont.textContent=0;
+       }
+       
+    
+   });
